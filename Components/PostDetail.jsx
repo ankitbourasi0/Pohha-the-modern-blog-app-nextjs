477dbb81b0761ab471df945cba41e719b300c5c6
Ankit Bourasi <ankitbourasi0@gmail.com>
2023-08-19 15:47:04 +0530

    preetify ui

--- Components/PostDetail.jsx
+++ Components/PostDetail.jsx
@@ -1,37 +1,93 @@
 import React from "react";
 import moment from "moment";
 const PostDetail = ({ post }) => {
+  const mediaQueryStyle = {
+    pre: {
+      overflow: "auto", // Default value
+      maxWidth: "100%", // Default value
+    },
+  };
+
   const getContentFragment = (index, text, obj, type) => {
     let modifiedText = text;
 
     if (obj) {
       if (obj.bold) {
-        modifiedText = (<b key={index}>{text}</b>);
+        modifiedText = <b key={index}>{text}</b>;
       }
 
       if (obj.italic) {
-        modifiedText = (<em key={index}>{text}</em>);
+        modifiedText = <em key={index}>{text}</em>;
       }
 
       if (obj.underline) {
-        modifiedText = (<u key={index}>{text}</u>);
+        modifiedText = <u key={index}>{text}</u>;
       }
 
-      if(obj.code){
-        modifiedText = ( <pre  key={index}><code className="font-medium text-xl">{text}</code></pre>);
+      if (obj.code) {
+        modifiedText = (
+          <pre
+            style={{
+              overflow:
+                typeof window !== "undefined" && window.innerWidth >= 768
+                  ? "unset"
+                  : "auto",
+              maxWidth:
+                typeof window !== "undefined" && window.innerWidth >= 768
+                  ? "unset"
+                  : "100%",
+              padding: "10px",
+
+              border: " 1px solid #ddd",
+              borderRadius: "4px",
+              whiteSpace: "pre-wrap",
+              wordWrap: "break-word",
+              fontFamily: "Consolas, Monaco, monospace",
+              fontSize: "14px",
+              ...mediaQueryStyle.pre,
+            }}
+            key={index}
+          >
+            <code
+              style={{
+                display: "block",
+              }}
+              className="font-medium text-xl "
+            >
+              {text}
+            </code>
+          </pre>
+        );
       }
     }
 
-
     switch (type) {
-      case 'heading-three':
-        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
-      case 'paragraph':
-        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
-     
-      case 'heading-four':
-        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
-      case 'image':
+      case "heading-three":
+        return (
+          <h3 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        );
+      case "paragraph":
+        return (
+          <p key={index} className="mb-8 text-lg  text-justify">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        );
+
+      case "heading-four":
+        return (
+          <h4 key={index} className="text-md font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        );
+      case "image":
         return (
           <img
             key={index}
@@ -46,7 +102,6 @@ const PostDetail = ({ post }) => {
     }
   };
 
-
   return (
     <div className="bg-white dark:bg-transparent lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow mb-6 ">
@@ -56,7 +111,7 @@ const PostDetail = ({ post }) => {
           className="object-top h-full w-full rounded-t-lg"
         />
       </div>
-      <div className="px-4 lg:px-0 ">
+      <div className="px-4 lg:px-0  ">
         <div className="flex  items-center mb-8 w-full">
           <div className="flex items-center  mb-4 lg:mb-0 w-full lg:w-auto mr-8 ">
             <img
@@ -88,10 +143,12 @@ const PostDetail = ({ post }) => {
             <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
           </div>
         </div>
-        <h1  className="mb-8 text-3xl font-semibold">{post.title}</h1>
-        {post.content.raw.children.map((typeObj,index) =>{
-          const children = typeObj.children.map((item,itemIndex)=>getContentFragment(itemIndex,item.text,item))
-          return getContentFragment( index,children,typeObj,typeObj.type)
+        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+        {post.content.raw.children.map((typeObj, index) => {
+          const children = typeObj.children.map((item, itemIndex) =>
+            getContentFragment(itemIndex, item.text, item)
+          );
+          return getContentFragment(index, children, typeObj, typeObj.type);
         })}
       </div>
     </div>
